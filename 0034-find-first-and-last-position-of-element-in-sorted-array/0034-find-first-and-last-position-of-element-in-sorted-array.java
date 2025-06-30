243a2134cbc49df5eb6f05fc721ec07b5b6308f0
Bhaumik182001 <Bhaumik182001@gmail.com>
2025-06-30 10:54:33 +0530

    Time: 0 ms (100%), Space: 46.5 MB (8.5%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,64 +1,48 @@
 class Solution {
-    public int bLow(int[] nums, int target){
+    int high(int[] nums, int target){
+        int low = 0;
+        int high = nums.length - 1;
         int res = -1;
-        
-        int l = 0;
-        int h = nums.length - 1;
-        int mid = 0;
 
-        while(l <= h){
-            mid = (l+h)/2;
+        while(low <= high){
+            int mid = (low + high)/2;
             if(nums[mid] == target){
                 res = mid;
-                h = mid - 1;
-            }
-            else if(nums[mid] < target){
-                
-                l = mid + 1;
+                low = mid + 1;
+            }else if(target < nums[mid]){
+                high = mid - 1;
             } else {
-                h = mid - 1;
+                low = mid + 1;
             }
         }
 
-        
         return res;
     }
 
-    public int bHigh(int[] nums, int target){
+    int low(int[] nums, int target){
+        int low = 0;
+        int high = nums.length - 1;
         int res = -1;
-        
-        int l = 0;
-        int h = nums.length - 1;
-        int mid = 0;
 
-        while(l <= h){
-            mid = (l+h)/2;
-
+        while(low <= high){
+            int mid = (low + high)/2;
             if(nums[mid] == target){
                 res = mid;
-                l = mid + 1;
-            }
-            else if(nums[mid] > target){
-                
-                h = mid - 1;
+                high = mid - 1;
+            }else if(target < nums[mid]){
+                high = mid - 1;
             } else {
-              
-                l = mid + 1;
+                low = mid + 1;
             }
         }
 
-        
         return res;
     }
 
     public int[] searchRange(int[] nums, int target) {
-       
-        
-        int[] res = new int[2];
-    
-        res[0] = bLow(nums, target);
-        res[1] = bHigh(nums, target);
-
+        int[] res = {0, 0};
+        res[0] = low(nums, target);
+        res[1] = high(nums, target);
         return res;
     }
 }
