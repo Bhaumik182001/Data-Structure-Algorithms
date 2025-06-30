43e0165d894dd26e3aee4ec7432b911d49c84e4d
Bhaumik182001 <Bhaumik182001@gmail.com>
2025-06-30 09:40:12 +0530

    Time: 11 ms (75.5%), Space: 47.3 MB (59.11%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,40 +1,41 @@
 class Solution {
-    boolean possible(int[] arr, int n, int days, int cap){
+    boolean possible(int[] arr, int days, int capacity){
         int sum = 0;
         int day = 1;
 
-        for(int i = 0; i < n; i++){
-            if((sum + arr[i]) <= cap) sum += arr[i];
-            else {
+        for(int i = 0; i < arr.length; i++){
+            if((sum + arr[i]) <= capacity){
+                sum += arr[i];
+            } else {
                 sum = arr[i];
                 day++;
             }
         }
 
-        if(day <= days) return true;
-        return false;
+        return day <= days;
     }
-
+    
     public int shipWithinDays(int[] weights, int days) {
         int low = 0;
         int high = 0;
-     
-        for(int num: weights){
-            low = Math.max(low, num);
-            high += num;
-        }
 
         int ans = 0;
 
-        while(low <= high){ 
-            int mid = (low + high)/2;
-            if(possible(weights, weights.length, days, mid)){
+        for(int weight: weights){
+            high += weight;
+            low = Math.max(low, weight);
+        }
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+
+            if(possible(weights, days, mid)){
                 ans = mid;
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
-        return ans;
+       return ans;
     }
 }
