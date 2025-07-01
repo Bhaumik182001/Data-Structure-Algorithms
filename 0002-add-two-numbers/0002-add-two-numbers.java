484de1a2e6b68b0bd3b8ae063a20c9e6ce692b84
Bhaumik182001 <Bhaumik182001@gmail.com>
2025-07-01 12:44:26 +0530

    Time: 1 ms (100%), Space: 44.4 MB (83.1%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,44 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int carry = 0;
         ListNode p = l1;
         ListNode q = l2;
         ListNode r = new ListNode();
         ListNode temp = r;
 
-        while(p != null && q != null){
-            int sum = p.val + q.val + carry;
-            carry = sum / 10;
+        int carry = 0;
+
+        while(p != null || q != null || carry != 0){
+            int sum = 0;
+
+            if(p != null){
+                sum += p.val;
+                p = p.next;
+            }
+
+            if(q != null){
+                sum += q.val;
+                q = q.next;
+            }
+
+            sum += carry;
+
             ListNode newNode = new ListNode(sum % 10);
             temp.next = newNode;
             temp = temp.next;
-            p = p.next;
-            q = q.next;
-        }
 
-        while(p != null){
-            int sum =p.val + carry;
-            ListNode newNode = new ListNode(sum % 10);
             carry = sum / 10;
-            temp.next = newNode;
-            temp = temp.next;
-            p = p.next;
-        }
-
-        while(q != null){
-            int sum = q.val + carry;
-            ListNode newNode = new ListNode(sum % 10);
-            carry = sum / 10;
-            temp.next = newNode;
-            temp = temp.next;
-            q = q.next;
-        }
-
-        while(carry != 0){
-            ListNode newNode = new ListNode(carry % 10);
-            temp.next = newNode;
-            temp = temp.next;
-            carry /= 10;
         }
 
         return r.next;
