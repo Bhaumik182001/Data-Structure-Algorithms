a09855041a8ec360570803621cdd16784c447fe2
Bhaumik182001 <Bhaumik182001@gmail.com>
2025-07-02 09:37:54 +0530

    Time: 0 ms (100%), Space: 42 MB (68.95%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -15,23 +15,23 @@ class Solution {
         ListNode dummy = new ListNode(-1, head);
         ListNode temp = dummy;
 
-        // Move `temp` to the last node with value < x
+     
         while (temp.next != null && temp.next.val < x) {
             temp = temp.next;
         }
 
         ListNode curr = temp;
-        // Start from where `temp` left off and check next nodes
+     
         while (curr.next != null) {
             if (curr.next.val < x) {
-                // Move the node to after `temp`
+                
                 ListNode move = curr.next;
                 curr.next = move.next;
 
                 move.next = temp.next;
                 temp.next = move;
 
-                temp = temp.next;  // advance insertion point
+                temp = temp.next;  
             } else {
                 curr = curr.next;
             }
