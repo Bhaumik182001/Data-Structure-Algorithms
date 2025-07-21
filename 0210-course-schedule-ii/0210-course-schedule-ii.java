d4fb6b39577d7bd081d9b8fed2860c2b2eba5d04
Bhaumik <Bhaumik182001@gmail.com>
2025-07-21 12:23:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int[] findOrder(int V, int[][] edges) {
+        ArrayList<Integer>[] adj = new ArrayList[V];
+
+        for(int i = 0; i < V; i++){
+            adj[i] = new ArrayList<>();
+        }
+
+        for(int[] edge: edges){
+            adj[edge[0]].add(edge[1]);
+        }
+
+        int[] indegree = new int[V];
+
+        for(int i = 0; i < V; i++){
+            for(int num: adj[i]){
+                indegree[num]++;
+            }
+        }
+
+        ArrayList<Integer> res = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i = 0; i < V; i++){
+            if(indegree[i] == 0){
+                q.offer(i);
+            }
+        }
+
+        while(!q.isEmpty()){
+            int val = q.remove();
+            res.add(val);
+        
+
+        for(int num: adj[val]){
+            indegree[num]--;
+            if(indegree[num] == 0){
+                q.offer(num);
+            }
+        }
+
+        }
+
+        Collections.reverse(res);
+
+        int[] ans = res.stream()
+                              .mapToInt(Integer::intValue)
+                              .toArray();
+
+        return ans;
+    }
+}
