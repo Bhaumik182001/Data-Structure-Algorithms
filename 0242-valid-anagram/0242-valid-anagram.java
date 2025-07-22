e81c19d8d8c34605205ffd396101ade7be53c6b5
Bhaumik <Bhaumik182001@gmail.com>
2025-07-22 10:07:23 +0530

    Time: 21 ms (10.19%), Space: 44.6 MB (42.93%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,11 +1,29 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] alphabets = new int[26];
+       if(s.length() != t.length()) return false;
+       
+       HashMap <Character, Integer> hs = new HashMap<>();
+    
+        for(int i = 0; i < s.length(); i++){
+            if(!hs.containsKey(s.charAt(i))){
+                hs.put(s.charAt(i), 1);
+            } else {
+                hs.put(s.charAt(i), hs.get(s.charAt(i)) + 1);
+            }
+        }
 
-        for(int i = 0; i < s.length(); i++) alphabets[s.charAt(i) - 'a']++;
-        for(int i = 0; i < t.length(); i++) alphabets[t.charAt(i) - 'a']--;
+        for(int i = 0; i < t.length(); i++){
+            if(!hs.containsKey(t.charAt(i)) || hs.get(t.charAt(i)) == 0){
+                return false;
+            } else {
+                hs.put(t.charAt(i), hs.get(t.charAt(i)) - 1);
+            }
+        }
 
-        for(int num: alphabets) if(num != 0) return false;
+        for(Character c: hs.keySet()){
+            if(hs.get(c) != 0) return false;
+        }
+       
 
         return true;
     }
