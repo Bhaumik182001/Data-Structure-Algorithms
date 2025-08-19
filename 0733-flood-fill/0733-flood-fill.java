6161627182575c6bfcff5cebb088a0fe787501b4
Bhaumik <Bhaumik182001@gmail.com>
2025-08-19 12:17:23 +0530

    Time: 0 ms (100%), Space: 45.5 MB (5.4%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,23 +1,17 @@
 class Solution {
-    void dfs(int[][] image, int i, int j, int r, int c, int old, int color){
-        if(i == r || j == c || i < 0 || j < 0 || image[i][j] != old || image[i][j] == color) return;
-
+    void dfs(int[][] image, int i, int j, int old, int color){
+        if(i < 0 || j < 0 || i == image.length || j == image[0].length || image[i][j] != old || image[i][j] == color) return;
+        
         image[i][j] = color;
-        
-        dfs(image, i, j + 1, r, c, old, color);
-        dfs(image, i - 1, j, r, c, old, color);
-        dfs(image, i, j - 1, r, c, old, color);
-        dfs(image, i + 1, j, r, c, old, color);
 
-        
+        dfs(image, i , j + 1, old, color);
+        dfs(image, i - 1 , j, old, color);
+        dfs(image, i , j - 1, old, color);
+        dfs(image, i + 1 , j, old, color);
     }
-    
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int r = image.length;
-        int c = image[0].length;
 
-        int old = image[sr][sc];
-        dfs(image, sr, sc, r, c, old, color);
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        dfs(image, sr, sc, image[sr][sc], color);
         return image;
     }
 }
