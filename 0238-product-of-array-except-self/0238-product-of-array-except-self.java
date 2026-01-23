b7b091b14de2d927b52eb2e216041f4866d16404
Bhaumik <Bhaumik182001@gmail.com>
2026-01-23 10:18:14 +0530

    Time: 3 ms (27.34%), Space: 65 MB (83.6%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,29 +1,27 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
+        int[] prefix = new int[nums.length];
+        int[] postfix = new int[nums.length];
         int[] ans = new int[nums.length];
-        int[] pre = new int[nums.length];
-        int[] post = new int[nums.length];
+        int n = nums.length;
 
-        int prefix = 1;
-        int postfix = 1;
-        
+        int pre = 1;
+        int post = 1;
+
         for(int i = 0; i < n; i++){
-            prefix *= nums[i];
-            postfix *= nums[n-i-1];
-            pre[i] = prefix;
-            post[n-i-1] = postfix;
+            pre *= nums[i];
+            post *= nums[n - i - 1];
+            prefix[i] = pre;
+            postfix[n - i - 1] = post;
         }
 
-       
-        ans[0] = post[1];
-        ans[n-1] = pre[n-2];
+        ans[0] = postfix[1];
+        ans[n - 1] = prefix[n - 2];
 
-        for(int i = 1; i < n-1; i++){
-            ans[i] = pre[i-1]*post[i+1];
+        for(int i = 1; i < n - 1; i++){
+            ans[i] = prefix[i - 1] * postfix[i + 1];
         }
 
-
-        return ans;     
+        return ans;
     }
 }
