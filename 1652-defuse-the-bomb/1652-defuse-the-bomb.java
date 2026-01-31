22c58e698d992808b2799caa61eb3e57019b9d64
Bhaumik <Bhaumik182001@gmail.com>
2026-01-31 10:08:08 +0530

    Time: 43 ms (4.7%), Space: 52.8 MB (11.55%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int len = code.length;
+        int[] ans = new int[len];
+        if(k == 0) {
+            for(int i = 0; i < code.length; i++){
+                code[i] = 0;
+            }
+            return code;
+        }
+
+        int sum = 0;
+        int left = -1;
+        int right = -1;
+
+        if(k > 0){
+            for(int i = 1; i <= k; i++){
+                sum += code[i];
+            }
+            left = 1;
+            right = k;
+        } else {
+            for(int i = len + k; i < len; i++){
+                sum += code[i];
+            }
+            left = len + k;
+            right = len - 1;
+        }
+
+        for(int i = 0; i < code.length; i++){
+            ans[i] = sum;
+            System.out.println("code[left]: " + code[left] + " sum: " + sum + " left: " + left);
+            sum -= code[left++];
+            left %= len;
+            System.out.println("left: " + left + " sum: " + sum);
+            right++;
+            right %= len;
+            System.out.println("right: " + right + " sum: " + sum);
+            sum += code[right];
+            System.out.println("code[right]: " + code[right] + " sum: " + sum + " right: " + left);
+        }
+
+        return ans;
+    }
+}
