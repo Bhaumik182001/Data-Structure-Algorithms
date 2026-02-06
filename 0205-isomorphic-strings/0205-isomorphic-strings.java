3e94a4bc919161ed729fb67c7e9dd4648059c836
Bhaumik <Bhaumik182001@gmail.com>
2026-02-06 09:18:41 +0530

    Time: 12 ms (53.36%), Space: 44.2 MB (15.08%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -6,15 +6,20 @@ class Solution {
         for(int i = 0; i < s.length(); i++){
             char a = s.charAt(i);
             char b = t.charAt(i);
-            
             if(hm1.containsKey(a)){
                 if(hm1.get(a) != b)return false;
             } else hm1.put(a, b);
-
-            if(hm2.containsKey(b)){
-                if(hm2.get(b) != a)return false;
-            } else hm2.put(b, a);
         }
+
+        for(int i = 0; i < s.length(); i++){
+            char b = s.charAt(i);
+            char a = t.charAt(i);
+            if(hm2.containsKey(a)){
+                if(hm2.get(a) != b)return false;
+            } else hm2.put(a, b);
+        }
+
+
 
         return true;
     }
