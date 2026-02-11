4a3fec2dbe92cfcf4eca9ed0d2e984ca94ee6ba3
Bhaumik <Bhaumik182001@gmail.com>
2026-02-11 14:09:44 +0530

    Time: 39 ms (74.51%), Space: 76 MB (82.35%) - LeetHub

--- 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.java
+++ 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.java
@@ -1,43 +1,4 @@
-import java.util.*;
-
 class Solution {
-    public long countPairs(int[] nums, int k) {
-        long count = 0;
-        Map<Integer, Long> gcdFreq = new HashMap<>();
-
-        // Step 1: Count frequencies of GCDs
-        for (int num : nums) {
-            int common = gcd(num, k);
-            gcdFreq.put(common, gcdFreq.getOrDefault(common, 0L) + 1);
-        }
-
-        // Get unique GCDs to avoid nested loops over the whole array
-        List<Integer> uniqueGcds = new ArrayList<>(gcdFreq.keySet());
-
-        // Step 2: Compare pairs of unique GCDs
-        for (int i = 0; i < uniqueGcds.size(); i++) {
-            int g1 = uniqueGcds.get(i);
-            long freq1 = gcdFreq.get(g1);
-
-            // Case A: Pairing a GCD with itself (e.g., g1 * g1 % k == 0)
-            if (((long)g1 * g1) % k == 0) {
-                count += (freq1 * (freq1 - 1)) / 2;
-            }
-
-            // Case B: Pairing with a different GCD
-            for (int j = i + 1; j < uniqueGcds.size(); j++) {
-                int g2 = uniqueGcds.get(j);
-                long freq2 = gcdFreq.get(g2);
-
-                if (((long)g1 * g2) % k == 0) {
-                    count += freq1 * freq2;
-                }
-            }
-        }
-
-        return count;
-    }
-
     private int gcd(int a, int b) {
         while (b != 0) {
             a %= b;
@@ -46,5 +7,33 @@ class Solution {
             b = temp;
         }
         return a;
+    }
+
+    public long countPairs(int[] nums, int k) {
+        long count = 0;
+        HashMap<Integer, Long> freq = new HashMap<>();
+
+        for(int num: nums){
+            int common = gcd(num, k);
+            freq.put(common, freq.getOrDefault(common, 0L) + 1);
+        }
+
+        List<Integer> unique = new ArrayList<>(freq.keySet());
+
+        for(int i = 0; i < unique.size(); i++){
+            int num1 = unique.get(i);
+            long freq1 = freq.get(num1);
+
+            if(((long)num1 * num1) % k == 0) count += ((freq1) * (freq1 - 1)) / 2;
+
+            for(int j = i + 1; j < unique.size(); j++){
+                int num2 = unique.get(j);
+                long freq2 = freq.get(num2);
+
+                if(((long)num2 * num1) % k == 0) count += (freq1 * freq2);
+            }
+        }
+
+        return count;
     }
 }
