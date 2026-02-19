3554de962d183fcf76a9de6b04b51fc04b9d0581
Bhaumik <Bhaumik182001@gmail.com>
2026-02-19 11:28:53 +0530

    Time: 8 ms (17.54%), Space: 43.1 MB (99.91%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -14,22 +14,25 @@
  * }
  */
 class Solution {
-    String temp = "";
-
-    void sol(TreeNode root, List<String> res, String temp){
+    void recur(TreeNode root, List<String> res, String temp){
         if(root == null) return;
-        temp += root.val;
-        if(root.left == null && root.right == null) res.add(temp);
-        else temp += "->";
 
-        sol(root.left, res, temp);
-        sol(root.right, res, temp);
+        temp += root.val;
+
+        if(root.left == null && root.right == null){
+            res.add(temp);
+            return;
+        }
+
+        temp += "->";
+
+        recur(root.left, res, temp);
+        recur(root.right, res, temp);
     }
 
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> res = new ArrayList<>();
- 
-        sol(root, res, temp);
+        recur(root, res, "");
         return res;
     }
 }
