2f68241ed4faa1b202bee4c105f50339f75dfbf9
Bhaumik <Bhaumik182001@gmail.com>
2026-02-24 11:24:09 +0530

    Time: 12 ms (12.63%), Space: 61.9 MB (21.47%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -0,0 +1,58 @@
+class Solution {
+    int count = 0;
+
+    void dfsB(int[][] grid, int i, int j, int r, int c){
+        if(i == r || j == c || i < 0 || j < 0 || grid[i][j] != 1) return;
+
+        grid[i][j] = -1;
+
+        dfsB(grid, i, j + 1, r, c);
+        dfsB(grid, i - 1, j, r, c);
+        dfsB(grid, i, j - 1, r, c);
+        dfsB(grid, i + 1, j, r, c);
+    }
+
+    void dfs(int[][] grid, int i, int j, int r, int c){
+        if(i == r || j == c || i < 0 || j < 0 || grid[i][j] == 1) return;
+
+        grid[i][j] = 1;
+
+        dfs(grid, i, j + 1, r, c);
+        dfs(grid, i - 1, j, r, c);
+        dfs(grid, i, j - 1, r, c);
+        dfs(grid, i + 1, j, r, c);
+    }
+
+
+    public int numEnclaves(int[][] grid) {
+        int r = grid.length;
+        int c = grid[0].length;
+
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                boolean isBorder = (i == 0 || j == 0 || i == (r - 1) || j == (c - 1));
+                if(grid[i][j] == 1 && isBorder){
+                    dfsB(grid, i, j, r, c);
+                }  
+
+            }
+        }
+
+
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                boolean isBorder = (i == 0 || j == 0 || i == (r - 1) || j == (c - 1));
+                if(grid[i][j] == 1 && !isBorder){
+                    dfs(grid, i, j, r, c);
+                    count++;
+                }  
+
+            }
+        }
+
+        return count;
+
+        
+    }
+    
+}
