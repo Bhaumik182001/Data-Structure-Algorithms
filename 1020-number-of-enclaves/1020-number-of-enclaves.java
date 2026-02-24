611daf8d176485a044eb5d2400e5101777142838
Bhaumik <Bhaumik182001@gmail.com>
2026-02-24 11:40:57 +0530

    Time: 12 ms (12.63%), Space: 61.8 MB (24.91%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -15,7 +15,7 @@ class Solution {
     void dfs(int[][] grid, int i, int j, int r, int c){
         if(i == r || j == c || i < 0 || j < 0 || grid[i][j] == 1) return;
 
-        grid[i][j] = 1;
+        grid[i][j] = 0;
 
         dfs(grid, i, j + 1, r, c);
         dfs(grid, i - 1, j, r, c);
