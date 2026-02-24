5f307278c76e282fadc4a26f010836a0dbbb1625
Bhaumik <Bhaumik182001@gmail.com>
2026-02-24 10:36:48 +0530

    Time: 2 ms (99.83%), Space: 52.4 MB (58.98%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,8 +1,9 @@
 class Solution {
     void dfs(char[][] grid, int i, int j, int r, int c){
         if(i == r || j == c || i < 0 || j < 0 || grid[i][j] == '0') return;
-        
+
         grid[i][j] = '0';
+
         dfs(grid, i, j + 1, r, c);
         dfs(grid, i - 1, j, r, c);
         dfs(grid, i, j - 1, r, c);
@@ -17,11 +18,10 @@ class Solution {
 
         for(int i = 0; i < r; i++){
             for(int j = 0; j < c; j++){
-                if(grid[i][j] == '1'){
+                if(grid[i][j] == '1') {
                     dfs(grid, i, j, r, c);
                     ans++;
-                } 
-
+                }
             }
         }
 
