5f6b1431f67d135154f07f67f1dc3949f41db575
Bhaumik <Bhaumik182001@gmail.com>
2026-02-24 11:00:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,58 @@
+class Solution {
+    void dfsB(char[][] grid, int i, int j, int r, int c){
+        if(i == r || j == c || i < 0 || j < 0 || grid[i][j] != 'O') return;
+
+        grid[i][j] = 'S';
+
+        dfsB(grid, i, j + 1, r, c);
+        dfsB(grid, i - 1, j, r, c);
+        dfsB(grid, i, j - 1, r, c);
+        dfsB(grid, i + 1, j, r, c);
+    }
+
+    void dfs(char[][] grid, int i, int j, int r, int c){
+        if(i == r || j == c || i < 0 || j < 0 || grid[i][j] != 'O') return;
+
+        grid[i][j] = 'X';
+
+        dfs(grid, i, j + 1, r, c);
+        dfs(grid, i - 1, j, r, c);
+        dfs(grid, i, j - 1, r, c);
+        dfs(grid, i + 1, j, r, c);
+    }
+
+    void dfsR(char[][] grid, int i, int j, int r, int c){
+        if(i == r || j == c || i < 0 || j < 0 || grid[i][j] != 'S') return;
+
+        grid[i][j] = 'O';
+
+        dfsR(grid, i, j + 1, r, c);
+        dfsR(grid, i - 1, j, r, c);
+        dfsR(grid, i, j - 1, r, c);
+        dfsR(grid, i + 1, j, r, c);
+    }
+
+    public void solve(char[][] grid) {
+        int r = grid.length;
+        int c = grid[0].length;
+
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                if(grid[i][j] == 'O' && (i < (r - 1) && j < (c - 1) && i > 0 && j > 0)){
+                    dfs(grid, i, j, r, c);
+                } else if(grid[i][j] == 'O'){
+                    dfsB(grid, i, j, r, c);
+                }  
+
+            }
+        }
+
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                if(grid[i][j] == 'S'){
+                    dfsR(grid, i, j, r, c);
+                } 
+            }
+        }
+    }
+}
