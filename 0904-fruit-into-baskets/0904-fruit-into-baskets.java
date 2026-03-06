c319b70fffb7eb896ed51c41a4375b69690462f8
Bhaumik <Bhaumik182001@gmail.com>
2026-03-06 10:41:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,23 +1,17 @@
 class Solution {
-    public int totalFruit(int[] fruits) {
+    public int totalFruit(int[] nums) {
         HashMap<Integer, Integer> hm = new HashMap<>();
         int res = 0;
         int left = 0;
+       
 
-        for (int right = 0; right < fruits.length; right++) {
-            
-            hm.put(fruits[right], hm.getOrDefault(fruits[right], 0) + 1);
-
-            
-            while (hm.size() > 2) {
-                hm.put(fruits[left], hm.get(fruits[left]) - 1);
-                if (hm.get(fruits[left]) == 0) {
-                    hm.remove(fruits[left]);
-                }
+        for(int right = 0; right < nums.length; right++){
+            hm.put(nums[right], hm.getOrDefault(nums[right], 0) + 1);
+            while(hm.size() > 2){
+                hm.put(nums[left], hm.get(nums[left]) -1);
+                if(hm.get(nums[left]) == 0) hm.remove(nums[left]);
                 left++;
             }
-
-            
             res = Math.max(res, right - left + 1);
         }
 
