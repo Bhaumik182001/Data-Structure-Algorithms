cd0357d0302962f2941ebe6560d86654ab697944
Bhaumik <Bhaumik182001@gmail.com>
2026-03-17 16:20:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -2,14 +2,13 @@ class Solution {
     public long countCommas(long n) {
         long commas = 0;
         long limit = 999;
-        
-        while (n > limit) {
+
+        while(n > limit){
             commas += (n - limit);
-            if (limit > Long.MAX_VALUE / 1000) break;
-            
-            limit = limit * 1000 + 999; 
+            if(limit > Long.MAX_VALUE / 1000) break;
+            limit = limit * 1000 + 999;
         }
-        
+
         return commas;
     }
 }
