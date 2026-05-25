c3d44df723787be2257f2670a6d3002b15987f8e
Bhaumik <Bhaumik182001@gmail.com>
2026-05-25 10:27:20 +0530

    Time: 0 ms (100%), Space: 42.3 MB (97.66%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -22,8 +22,7 @@ class Solution {
     }
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null) return true;
-        if(p == null || q == null) return false;
+        
         
         return clone(p, q);
     }
