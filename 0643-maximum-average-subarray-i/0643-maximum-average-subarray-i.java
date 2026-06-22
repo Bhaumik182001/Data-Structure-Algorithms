3a2b60ae163e9a4708be16405ea78971f7cdee7d
Bhaumik <Bhaumik182001@gmail.com>
2026-06-22 11:51:05 +0530

    Time: 2 ms (99.83%), Space: 69.7 MB (24.8%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,9 +1,8 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-          
         
-
         int sum = 0;
+
         for(int i = 0; i < k; i++){
             sum += nums[i];
         }
@@ -11,7 +10,7 @@ class Solution {
         int max = sum;
 
         for(int i = k; i < nums.length; i++){
-            sum += nums[i] - nums[i -k];
+            sum += nums[i] - nums[i - k];
             max = Math.max(max, sum);
         }
 
