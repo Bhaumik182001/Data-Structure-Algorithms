a246c76236d3ec744b6e2991328deb886249ee35
Bhaumik <Bhaumik182001@gmail.com>
2026-06-26 10:38:08 +0530

    Time: 1 ms (83.32%), Space: 42.8 MB (54.6%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,26 +1,33 @@
 class Solution {
-    public boolean wordPattern(String pattern, String s) {
-        String[] str = s.split(" ");
-        if(str.length != pattern.length()) return false;
-       
+    public boolean wordPattern(String pattern, String j) {
+        String s = pattern;
+        String[] t = j.split(" ");
+
+        if(s.length() != t.length) return false;
+
         HashMap<Character, String> hm1 = new HashMap<>();
         HashMap<String, Character> hm2 = new HashMap<>();
 
+        for(int i = 0; i < s.length(); i++){
+            char a = s.charAt(i);
+            String b = t[i];
 
-        for(int i = 0; i < pattern.length(); i++){
-            char a = pattern.charAt(i);
-            String b = str[i];
             if(hm1.containsKey(a)){
-                if(!hm1.get(a).equals(b))return false;
-            } else hm1.put(a, b);
+                if(!hm1.get(a).equals(b)) return false;
+            } else {
+                hm1.put(a, b);
+            }
         }
 
-         for(int i = 0; i < pattern.length(); i++){
-            char a = pattern.charAt(i);
-            String b = str[i];
+        for(int i = 0; i < t.length; i++){
+            char a = s.charAt(i);
+            String b = t[i];
+
             if(hm2.containsKey(b)){
-                if(hm2.get(b) != a)return false;
-            } else hm2.put(b, a);
+                if(hm2.get(b) != a) return false;
+            } else {
+                hm2.put(b, a);
+            }
         }
 
         return true;
