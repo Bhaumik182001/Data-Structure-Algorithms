a1ab0c4b8fd48cd6cbc6b6f3e7851668949c6470
Bhaumik <Bhaumik182001@gmail.com>
2026-06-26 09:53:04 +0530

    Time: 20 ms (9.17%), Space: 46.9 MB (5.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,22 +1,26 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
-        
-        HashMap<Character, Integer> hm1 = new HashMap<>();
-        HashMap<Character, Integer> hm2 = new HashMap<>();
+
+        HashMap<Character, Integer> hm = new HashMap<>();
+       
 
         for(int i = 0; i < s.length(); i++){
-            char a = s.charAt(i);
-            char b = t.charAt(i);
-            
-            hm1.put(a, hm1.getOrDefault(a, 0) + 1);
-            hm2.put(b, hm2.getOrDefault(b, 0) + 1);
+            hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0) + 1);
         }
 
-        for(char c : hm1.keySet()){
-            if(!hm1.get(c).equals(hm2.get(c))) return false;
+        for(int i = 0; i < t.length(); i++){
+
+            if (!hm.containsKey(t.charAt(i))) {
+                return false;
+            }
+            hm.put(t.charAt(i), hm.get(t.charAt(i)) - 1);
+
+            if(hm.get(t.charAt(i)) == 0) hm.remove(t.charAt(i));
         }
 
-        return true;
+        
+
+        return hm.size() == 0;
     }
 }
