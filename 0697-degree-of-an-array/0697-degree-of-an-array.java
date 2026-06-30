971b7cc2aa0cfc2bd383a248c4a7b5b1b3807d0e
Bhaumik <Bhaumik182001@gmail.com>
2026-06-30 09:40:36 +0530

    Time: 50 ms (6.91%), Space: 49.8 MB (80.64%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.java
+++ 0697-degree-of-an-array/0697-degree-of-an-array.java
@@ -1,36 +1,34 @@
 class Solution {
     public int findShortestSubArray(int[] nums) {
-        HashMap<Integer, Integer> freq = new HashMap<>();
-        int maxDegree = 0;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int max = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
-            maxDegree = Math.max(maxDegree, freq.get(nums[i]));
+        for(int num : nums){
+            hm.put(num, hm.getOrDefault(num, 0) + 1);
+            max = Math.max(max, hm.get(num));
         }
 
         int minLen = nums.length;
-        for (int num : freq.keySet()) {
-            if (freq.get(num) == maxDegree) {
-                
 
+        for(int key : hm.keySet()){
+            if(hm.get(key) == max){
                 int left = 0;
                 int right = nums.length - 1;
 
-                for (int i = 0; i < nums.length; i++) {
-                    if (nums[i] == num) {
+                for(int i = 0; i < nums.length; i++){
+                    if(nums[i] == key){
                         left = i;
                         break;
                     }
                 }
 
-                for (int i = nums.length - 1; i >= 0; i--) {
-                    if (nums[i] == num) {
+                for(int i = nums.length - 1; i >= 0; i--){
+                    if(nums[i] == key){
                         right = i;
                         break;
                     }
                 }
 
-        
                 minLen = Math.min(minLen, right - left + 1);
             }
         }
