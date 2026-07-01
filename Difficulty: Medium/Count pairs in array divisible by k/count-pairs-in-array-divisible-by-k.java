e3c16ad9a35bb4a1dc14bbc95cd76306fd7096c2
Bhaumik <Bhaumik182001@gmail.com>
2026-07-01 10:16:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count pairs in array divisible by k/count-pairs-in-array-divisible-by-k.java
+++ Difficulty: Medium/Count pairs in array divisible by k/count-pairs-in-array-divisible-by-k.java
@@ -1,24 +1,19 @@
 class Solution {
-    public int countKdivPairs(int[] arr, int k) { // Note: Changed return type to long to handle large counts
-        long[] freq = new long[k]; // FIX: Use long array to avoid overflow during multiplication
+    public int countKdivPairs(int[] arr, int k) {
+        // code here
+        long[] freq = new long[k];
         long count = 0;
         
-        for (int num : arr) {
+        for(int num : arr){
             int rem = ((num % k) + k) % k;
             freq[rem]++;
         }
         
-        // Handle remainder 0
         count += (freq[0] * (freq[0] - 1)) / 2;
         
-        // Handle all other remainders
-        for (int i = 1; i <= k / 2; i++) {
-            if (i == k - i) {
-                count += (freq[i] * (freq[i] - 1)) / 2;
-            } else {
-                // FIX: Multiplied the groups together and changed 'k - 1' to 'k - i'
-                count += freq[i] * freq[k - i];
-            }
+        for(int i = 1; i <= k/2; i++){
+            if(i == k - i) count += (freq[i] * (freq[i] - 1)) / 2;
+            else count += freq[i] * freq[k - i];
         }
         
         return (int)count;
