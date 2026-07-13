f1f94ec16e0f73a704d3215ae3a59c736c9e7da1
Bhaumik <Bhaumik182001@gmail.com>
2026-07-13 10:54:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -4,25 +4,19 @@ class Solution {
         int high = nums.length - 1;
 
         while(low <= high){
-            int mid = (low + high) / 2;
+            int mid = low + (high - low) / 2;
 
             if(nums[mid] == target) return true;
 
-            if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+            if(nums[low] == nums[high]){
                 low++;
                 high--;
-            } else if(nums[low] <= nums[mid]){
-                if(nums[low] <= target && target <= nums[mid]){
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
+            }
+
+            if(nums[low] <= nums[mid]){
+                low = mid + 1;
             } else {
-                if(nums[mid] <= target && target <= nums[high]){
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
+                high = mid - 1;
             }
         }
 
